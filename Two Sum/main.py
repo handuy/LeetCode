5622fc99a0052d523357634823fa077d9ac5a1c4
Duy Nguyễn <duy@Duys-MacBook-Air.local>
2022-12-07 21:40:35 +0700

    solve two sum: begin quick sort

---
+++ Two Sum/main.py
@@ -0,0 +1,42 @@
+# Sort list 
+
+count = 0
+
+def quickSort(arr):
+    global count
+    count += 1
+
+    less = []
+    pivotList = []
+    more = []
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        for i in arr:
+            if i < pivot:
+                less.append(i)
+            elif i > pivot:
+                more.append(i)
+            else:
+                pivotList.append(i)
+        
+        print("count:", count, "pivotList:", pivotList)
+        print("count:", count, "less:", less)
+        print("count:", count, "more:", more)
+
+        less = quickSort(less)  
+        more = quickSort(more)
+        
+        return less + pivotList + more
+
+a = [4, 65, 2, -31, 0, 99, 83, 782, 1]
+a = quickSort(a)
+
+print("final", a)
+
+# For each item k in sorted list, find: target - k
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         return []
