d5278b3030a7219ea8121e2bbf0709d61ff14300
Duy Nguyễn <duy@Duys-MacBook-Air.local>
2022-12-11 14:53:54 +0700

    solve Two Sum using Hash Map

--- Two Sum/Readme.md
+++ Two Sum/Readme.md
@@ -3,3 +3,6 @@ Given an array of integers nums and an integer target, return indices of the two
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 You can return the answer in any order.
+
+Way 3-->Using HashMap -->O(n)
+keep on storing the elements that we reach , also keep on check if target-element is already present in the hashmap or not , if it is , then we found a pair
--- Two Sum/main.py
+++ Two Sum/main.py
@@ -1,43 +1,22 @@
-# Sort list 
+# a = [7,11,15,2]
+# target = 9
 
-count = 0
+# newDict = {}
 
-def quickSort(arr):
-    global count
-    count += 1
 
-    less = []
-    pivotList = []
-    more = []
-    if len(arr) <= 1:
-        print("count:", count, "arr:", arr)
-        return arr
-    else:
-        pivot = arr[0]
-        for i in arr:
-            if i < pivot:
-                less.append(i)
-            elif i > pivot:
-                more.append(i)
+# for index, val in enumerate(a):
+#     if newDict.get(target - val) == None:
+#         newDict[val] = index
+#     else:
+#         print(index, val, newDict)
+#         print( index, newDict[target - val] )
+#         break
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        newDict = {}
+        for index, val in enumerate(nums):
+            if newDict.get(target - val) == None:
+                newDict[val] = index
             else:
-                pivotList.append(i)
-        
-        print("count:", count, "pivotList:", pivotList)
-        print("count:", count, "less:", less)
-        print("count:", count, "more:", more)
-
-        less = quickSort(less)  
-        more = quickSort(more)
-        
-        return less + pivotList + more
-
-a = [4, 65, 2, -31, 0, 99, 83, 782, 1]
-a = quickSort(a)
-
-print("final", a)
-
-# For each item k in sorted list, find: target - k
-
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         return []
+                return [ index, newDict[target - val] ]
